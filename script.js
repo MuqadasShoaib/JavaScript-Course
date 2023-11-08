5ede8cdafb69c6b00cc22ae6d533343359d2ae6b
MuqadasShoaib <muqadas.shoaib@emumba.com>
2023-11-07 17:19:34 -0800

    today report
    11 nov 2023

---
+++ script.js
@@ -0,0 +1,21 @@
+import Backpack from "./file.js";
+
+
+const backpack = new Pack{
+     "Everyday Backpack",
+      30,
+     "grey",
+      15,
+      26,
+      26, 
+    }
+
+
+console.log("The backpack Object:", backpack);
+console.log("The backpack colour:", backpack.color);
+
+var query ="pocketNum";
+
+console.log("The pocketNum value:" , backpack[query]);
+
+export default backpack
